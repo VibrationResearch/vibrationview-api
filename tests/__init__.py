2cd4eeedc86d509b3099e9831f55a1bd4bd21718
Dan Van Baren <dan@vibrationresearch.com>
2025-05-05 08:55:16 -0400

    add __init__.py for structure

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+"""
+Test package for VibrationVIEW API.
+
+This package contains tests for the VibrationVIEW API wrapper.
+"""
+
+# This file primarily marks 'tests' as a Python package
+# Most test configuration is in conftest.py
