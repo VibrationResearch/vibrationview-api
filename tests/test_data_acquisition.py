2f6160a3b1236dbc571d82a60963c00ec247bbc2
Dan Van Baren <dan@vibrationresearch.com>
2025-09-24 16:48:31 -0400

    We can assume our test is sine after loading a sine test

--- tests/test_data_acquisition.py
+++ tests/test_data_acquisition.py
@@ -50,10 +50,6 @@ class TestDataAcquisition:
             if sine_test:
                 try:
                     self.vv.RunTest(sine_test)
-                    test_type = self.vv.TestType()
-                    if test_type != vvTestType.TEST_SINE:
-                        logger.warning("Not a SINE test")
-                        pytest.skip("Could not run a sine test, vectors will have unexpected results")
                     logger.info(f"Running sine test: {sine_test}")
                 except Exception as e:
                     error_info = ExtractComErrorInfo(e)
